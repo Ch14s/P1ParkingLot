263b8985ac81b5c01d0259c6d4a2c524200b6f48
Chias <48773656+Devmathias@users.noreply.github.com>
2025-11-25 11:08:06 +0100

    Added test for generation of vehicle

--- src/vehicle.c
+++ src/vehicle.c
@@ -3,7 +3,7 @@
 #include <stdbool.h>
 #include "vehicle.h"
 
-Vehicle generateVehicle(char licensePLate[8],VechileType vehicleType, bool isElectric, bool isHandicapped){
+Vehicle generateVehicle(char licensePLate[8],VehicleType vehicleType, bool isElectric, bool isHandicapped){
     //create struct Vehicle
     Vehicle v = {.licensePlate = licensePLate, .vehicleType = vehicleType, .isDisabled = isHandicapped, .isElectric = isElectric};
     return v;
