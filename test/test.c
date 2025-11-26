1f027d388e7e47f83ea285bf3d3938466b0dea9b
Chias <48773656+Devmathias@users.noreply.github.com>
2025-11-26 12:43:27 +0100

    Addedd test file

---
+++ test/test.c
@@ -0,0 +1,11 @@
+#include "../src/vehicle.h"
+#include "../src/menu.h"
+#include "../src/parkingLot.h"
+#include "../src/vehicle.h"
+#include <assert.h>
+#include <string.h>
+#include <stdio.h> 
+
+int main(){
+    
+}
