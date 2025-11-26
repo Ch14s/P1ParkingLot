cd003274f1446e67e9dfdbc13ac21171d12d3ff2
Chias <48773656+Ch14s@users.noreply.github.com>
2025-11-26 13:15:24 +0100

    Remove vehicle generation and output
    Removed vehicle generation and printing from main.

---
+++ test/test.c
@@ -0,0 +1,12 @@
+#include "../src/vehicle.h"
+#include "../src/menu.h"
+#include "../src/parkingLot.h"
+#include "../src/vehicle.h"
+#include <assert.h>
+#include <string.h>
+#include <stdio.h> 
+
+int main(){
+
+    assert(1==1);
+}
