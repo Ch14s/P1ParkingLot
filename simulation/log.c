0822bf0aa71f13ad45b2b8ab72fbca9810333a15
Chias <48773656+Devmathias@users.noreply.github.com>
2025-12-08 10:15:22 +0100

    Waing for more function to finish implementation

--- simulation/log.c
+++ simulation/log.c
@@ -5,13 +5,26 @@
 
 
 
-int clog(const char *path, const char* message){
+int customLog(const char *path, const char* prefix, const char* message){
     FILE *f = fopen(path,"a");
-    if (f==NULL)return EXIT_FAILURE;
+    if (f==NULL)
+    {
+        return EXIT_FAILURE;
+    }
 
-    fprintf(f,"\n%s",message);
+    fprintf(f,"\n%-10s%s",prefix,message);
 
     fclose(f);
     
     return EXIT_SUCCESS;
+}
+void clearLogFile(const char* path){
+    FILE *f = fopen(path,"w");
+    if (f==NULL)
+    {
+        return;
+    }
+    fprintf(f,"===BEGIN LOG===");
+
+    fclose(f);
 }
--- simulation/log.h
+++ simulation/log.h
@@ -5,10 +5,18 @@
  * @brief Function for logging to file
  * 
  * @param path Pointer to path
+ * @param prefix char[10] prefix for the log entry
  * @param message string message
  * 
  * @return  Returns 0 for successfull operation
  */
-void clog(const char *path, const char* message);
+int customLog(const char *path, const char* prefix, const char* message);
+
+/**
+ * @brief Clears log file
+ * 
+ * @param path to log file
+ */
+void clearLogFile(const char* path);
 
 #endif // C_LOG
